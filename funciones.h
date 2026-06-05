af00801cfabc7f2bd4c98d614fa64e67ed40978c
naimid-alvarez <129760491+naimid-alvarez@users.noreply.github.com>
2026-06-05 11:55:26 -0300

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CANT_ARTICULOS
+#define CANT_SUCURSALES
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
